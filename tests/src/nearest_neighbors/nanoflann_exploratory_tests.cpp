1788b9ee250a15453c7e3e1b54dbe4296473caf5
falcowinkler <falcowinkler@icloud.com>
2021-03-01 21:34:40 +0100

    l2norm implies squared distances. use l1 norm (for now). add extra test to check boundaries of radius search

--- tests/src/nearest_neighbors/nanoflann_exploratory_tests.cpp
+++ tests/src/nearest_neighbors/nanoflann_exploratory_tests.cpp
@@ -31,7 +31,7 @@ TEST_F(NanoflannTest, FindsNearestNeighbors) {
   const int dim = 2;
 
   // construct a kd-tree index:
-  typedef KDTreeSingleIndexAdaptor<L2_Simple_Adaptor<double, Flock>,
+  typedef KDTreeSingleIndexAdaptor<L1_Adaptor<double, Flock>,
                                    Flock,
                                    dim /* dim */
                                    > my_kd_tree_t;
@@ -43,8 +43,39 @@ TEST_F(NanoflannTest, FindsNearestNeighbors) {
   const double search_radius = static_cast<double>(0.1);
   std::vector<std::pair<size_t, double>> ret_matches;
 
-  const double query_pt[2] = {1, 2};
-
+  const double query_pt[2] = {1, 2.09};
   const size_t nMatches = index.radiusSearch(&query_pt[0], search_radius, ret_matches, params);
   EXPECT_EQ(nMatches, 2);
+}
+
+TEST_F(NanoflannTest, DoesNotReturnPointsOutOfSearchRadius) {
+    Flock flock;
+    flock.points.resize(2);
+    Point point1;
+    point1.x = 1;
+    point1.y = 2;
+    Point point2;
+    point2.x = 1;
+    point2.y = 2;
+
+    flock.points[0] = point1;
+    flock.points[1] = point2;
+
+    const int dim = 2;
+
+    // construct a kd-tree index:
+    typedef KDTreeSingleIndexAdaptor<L1_Adaptor<double, Flock>, Flock, dim /* dim */
+                                     >
+                 my_kd_tree_t;
+    my_kd_tree_t index(dim /*dim*/, flock, KDTreeSingleIndexAdaptorParams(10 /* max leaf */));
+    index.buildIndex();
+
+    nanoflann::SearchParams params;
+
+    const double                           search_radius = static_cast<double>(0.1);
+    std::vector<std::pair<size_t, double>> ret_matches;
+
+    const double query_pt[2] = {1, 2.19};
+    const size_t nMatches    = index.radiusSearch(&query_pt[0], search_radius, ret_matches, params);
+    EXPECT_EQ(nMatches, 0);
 }
