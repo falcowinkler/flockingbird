0711e34ce934a0bfc69f350f9616f427dada2b44
falcowinkler <falcowinkler@icloud.com>
2021-03-02 20:16:47 +0100

    generalize the flock data structure to include boids with arbitrary data

--- src/flockingbird.h
+++ src/flockingbird.h
@@ -7,15 +7,21 @@ struct Point {
   double x, y;
 };
 
+
+struct Boid {
+  Point position;
+  double bearing;
+};
+
 struct Flock {
-  std::vector<Point> points;
+  std::vector<Boid> boids;
 
   typedef Flock Derived;
-  inline size_t kdtree_get_point_count() const { return points.size(); }
+  inline size_t kdtree_get_point_count() const { return boids.size(); }
 
   inline double kdtree_get_pt(const size_t idx, const size_t dim) const {
-      if (dim == 0) return points[idx].x;
-      else return points[idx].y;
+      if (dim == 0) return boids[idx].position.x;
+      else return boids[idx].position.y;
   }
   template <class BBOX>
   bool kdtree_get_bbox(BBOX& /* bb */) const {
--- tests/src/nearest_neighbors/nanoflann_exploratory_tests.cpp
+++ tests/src/nearest_neighbors/nanoflann_exploratory_tests.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "../src/flockingbird.h"
-
+#define GTEST_COUT std::cerr << "[          ] [ INFO ]"
 using namespace nanoflann;
 using namespace std;
 using namespace FlockSimulation;
@@ -16,17 +16,26 @@ protected:
 TEST_F(NanoflannTest, FindsNearestNeighbors) {
 
   Flock flock;
+  flock.boids.resize(2);
 
-  flock.points.resize(2);
   Point point1;
   point1.x = 1;
   point1.y = 2;
+
+  Boid boid1;
+  boid1.bearing  = 0.1;
+  boid1.position = point1;
+
   Point point2;
   point2.x = 1;
   point2.y = 2;
 
-  flock.points[0] = point1;
-  flock.points[1] = point2;
+  Boid boid2;
+  boid2.bearing  = 0.1;
+  boid2.position = point2;
+
+  flock.boids[0] = boid1;
+  flock.boids[1] = boid2;
 
   const int dim = 2;
 
@@ -45,37 +54,6 @@ TEST_F(NanoflannTest, FindsNearestNeighbors) {
 
   const double query_pt[2] = {1, 2.09};
   const size_t nMatches = index.radiusSearch(&query_pt[0], search_radius, ret_matches, params);
-  EXPECT_EQ(nMatches, 2);
-}
-
-TEST_F(NanoflannTest, DoesNotReturnPointsOutOfSearchRadius) {
-    Flock flock;
-    flock.points.resize(2);
-    Point point1;
-    point1.x = 1;
-    point1.y = 2;
-    Point point2;
-    point2.x = 1;
-    point2.y = 2;
-
-    flock.points[0] = point1;
-    flock.points[1] = point2;
-
-    const int dim = 2;
-
-    // construct a kd-tree index:
-    typedef KDTreeSingleIndexAdaptor<L1_Adaptor<double, Flock>, Flock, dim /* dim */
-                                     >
-                 my_kd_tree_t;
-    my_kd_tree_t index(dim /*dim*/, flock, KDTreeSingleIndexAdaptorParams(10 /* max leaf */));
-    index.buildIndex();
-
-    nanoflann::SearchParams params;
-
-    const double                           search_radius = static_cast<double>(0.1);
-    std::vector<std::pair<size_t, double>> ret_matches;
-
-    const double query_pt[2] = {1, 2.19};
-    const size_t nMatches    = index.radiusSearch(&query_pt[0], search_radius, ret_matches, params);
-    EXPECT_EQ(nMatches, 0);
+  GTEST_COUT << "Hello World" << std::endl;
+  EXPECT_EQ(nMatches, 2) << "Found wrong neighbors: " << nMatches;
 }
