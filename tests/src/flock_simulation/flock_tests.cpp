fe4bd77e4cbfa2f59458586343a2d311840b2392
falcowinkler <falcowinkler@icloud.com>
2021-04-25 16:19:14 +0200

    add test for copy constructor

--- tests/src/flock_simulation/flock_tests.cpp
+++ tests/src/flock_simulation/flock_tests.cpp
@@ -2,6 +2,7 @@
 #include "gtest/gtest.h"
 
 using namespace std;
+using namespace FlockSimulation;
 
 class FlockingbirdTests : public ::testing::Test {
 
@@ -12,10 +13,10 @@ protected:
 };
 
 TEST_F(FlockingbirdTests, RandomInit) {
-    FlockSimulation::Flock flock(10, 10, 10);
+    Flock flock(10, 10, 10);
     EXPECT_EQ(flock.boids.size(), 10);
     for (int i = 0; i < 10; i++) {
-        FlockSimulation::Boid boid = flock.boids[i];
+        Boid boid = flock.boids[i];
         EXPECT_GE(boid.velocity.x, 0);
         EXPECT_LE(boid.velocity.x, 1);
         EXPECT_GE(boid.velocity.y, 0);
@@ -25,4 +26,20 @@ TEST_F(FlockingbirdTests, RandomInit) {
         EXPECT_GE(boid.position.y, 0);
         EXPECT_LE(boid.position.y, 10);
     }
+}
+
+
+TEST_F(FlockingbirdTests, CopyConstructors) {
+  const int N = 1000;
+  const int numTests = 10;
+  for (int testRun = 0; testRun < numTests; testRun++) {
+    Flock flock(N, 100, 100);
+    Flock copy(flock);
+    for (int i = 0; i < N; i++) {
+      EXPECT_EQ(flock.boids[i].position.x, copy.boids[i].position.x);
+      EXPECT_EQ(flock.boids[i].position.y, copy.boids[i].position.y);
+      EXPECT_EQ(flock.boids[i].velocity.x, copy.boids[i].velocity.x);
+      EXPECT_EQ(flock.boids[i].velocity.y, copy.boids[i].velocity.y);
+    }
+  }
 }
