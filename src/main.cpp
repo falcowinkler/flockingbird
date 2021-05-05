57b9ffe6d8ff334d22cc6baf3655073bf1717413
falcowinkler <falcowinkler@icloud.com>
2021-05-05 23:13:14 +0200

    making a simple window with gtk..

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <cairo.h>
+#include <gtkmm.h>
+#include <gtkmm/drawingarea.h>
 #include <gtkmm/window.h>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!" << "\n";
+class Point : public Gtk::DrawingArea {
+public:
+    Point(){};
+};
+
+int main(int argc, char* argv[]) {
+    Gtk::Main   kit(argc, argv);
+    Gtk::Window window;
+    window.set_title("Libgnomedbmm example window");
+    window.set_default_size(400, 400);
+    window.show_all();
+    kit.run(window);
+    return 0;
 }
