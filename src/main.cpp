04aee3d590b7864503d471d64d296f32dced8e61
falcowinkler <falcowinkler@icloud.com>
2021-05-05 23:31:51 +0200

    update to GTK3, add simple example from gtk / cairo tutorial

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,52 @@
 #include <cairo.h>
-#include <gtkmm.h>
-#include <gtkmm/drawingarea.h>
-#include <gtkmm/window.h>
-#include <iostream>
+#include <gtk/gtk.h>
+// TOOD: checkout https://zetcode.com/gfx/cairo/ 
+static void do_drawing(cairo_t *);
 
-class Point : public Gtk::DrawingArea {
-public:
-    Point(){};
-};
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, 
+    gpointer user_data)
+{
+  do_drawing(cr);
 
-int main(int argc, char* argv[]) {
-    Gtk::Main   kit(argc, argv);
-    Gtk::Window window;
-    window.set_title("Libgnomedbmm example window");
-    window.set_default_size(400, 400);
-    window.show_all();
-    kit.run(window);
-    return 0;
+  return FALSE;
+}
+
+static void do_drawing(cairo_t *cr)
+{
+  cairo_set_source_rgb(cr, 0, 0, 0);
+  cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
+      CAIRO_FONT_WEIGHT_NORMAL);
+  cairo_set_font_size(cr, 40.0);
+
+  cairo_move_to(cr, 10.0, 50.0);
+  cairo_show_text(cr, "Disziplin ist Macht.");    
+}
+
+
+int main(int argc, char *argv[])
+{
+  GtkWidget *window;
+  GtkWidget *darea;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+  darea = gtk_drawing_area_new();
+  gtk_container_add(GTK_CONTAINER(window), darea);
+
+  g_signal_connect(G_OBJECT(darea), "draw", 
+      G_CALLBACK(on_draw_event), NULL); 
+  g_signal_connect(window, "destroy",
+      G_CALLBACK(gtk_main_quit), NULL);
+
+  gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+  gtk_window_set_default_size(GTK_WINDOW(window), 400, 90); 
+  gtk_window_set_title(GTK_WINDOW(window), "GTK window");
+
+  gtk_widget_show_all(window);
+
+  gtk_main();
+
+  return 0;
 }
